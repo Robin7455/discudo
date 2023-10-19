c763bb4941dc18bb99011bc0849ce68ef648430a
Anmol Gangwar <anmolgangwar64@gmail.com>
2023-10-20 02:16:55 +0530

    fix :bug: remove className bug from radix component

--- components/ui/dialog.tsx
+++ components/ui/dialog.tsx
@@ -11,11 +11,10 @@ const Dialog = DialogPrimitive.Root;
 const DialogTrigger = DialogPrimitive.Trigger;
 
 const DialogPortal = ({
-  className,
+  // ts-ignore
+
   ...props
-}: DialogPrimitive.DialogPortalProps) => (
-  <DialogPrimitive.Portal className={cn(className)} {...props} />
-);
+}: DialogPrimitive.DialogPortalProps) => <DialogPrimitive.Portal {...props} />;
 DialogPortal.displayName = DialogPrimitive.Portal.displayName;
 
 const DialogOverlay = React.forwardRef<
